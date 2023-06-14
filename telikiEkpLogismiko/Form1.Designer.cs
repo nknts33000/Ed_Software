e7b207e825b209f25becdd5b24067566dec38875
nknts33000 <77843099+nknts33000@users.noreply.github.com>
2023-06-14 23:24:10 +0300

    added query handler

--- telikiEkpLogismiko/Form1.Designer.cs
+++ telikiEkpLogismiko/Form1.Designer.cs
@@ -99,7 +99,7 @@
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
             this.textBox1.Location = new System.Drawing.Point(583, 52);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(327, 22);
             this.textBox1.TabIndex = 4;
@@ -108,7 +108,7 @@
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Control;
             this.textBox2.Location = new System.Drawing.Point(583, 113);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(327, 22);
             this.textBox2.TabIndex = 5;
@@ -117,7 +117,7 @@
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Control;
             this.textBox3.Location = new System.Drawing.Point(583, 180);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(327, 22);
             this.textBox3.TabIndex = 6;
@@ -127,7 +127,7 @@
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.Control;
             this.textBox4.Location = new System.Drawing.Point(583, 251);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(327, 22);
             this.textBox4.TabIndex = 7;
@@ -136,7 +136,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(83, 326);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 8;
@@ -146,7 +146,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(215, 326);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 9;
@@ -157,13 +157,14 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(83, 389);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(217, 28);
             this.button3.TabIndex = 10;
             this.button3.Text = "Complete Registration";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -182,7 +183,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- telikiEkpLogismiko/Form1.cs
+++ telikiEkpLogismiko/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace telikiEkpLogismiko
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -31,6 +34,38 @@ namespace telikiEkpLogismiko
             label4.Visible = true;
             textBox3.Visible = true;
             textBox4.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            query_handler();
+           
+        }
+
+        public async void query_handler()
+        {
+
+            var con = new NpgsqlConnection(
+    connectionString: "Server=localhost;Port=5432;User Id=postgres;Password=6972419550n;Database=ed_software;");
+            con.Open();
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                cmd.CommandText = $"INSERT INTO users (username, password, email, cybersec, ai, ui) VALUES (@username, @password, @email, @cybersec, @ai, @ui );";
+                cmd.Parameters.AddWithValue("username", "nknts330");
+                cmd.Parameters.AddWithValue("password", "2505");
+                cmd.Parameters.AddWithValue("email", "nknts33000@gmail.com");
+                //cmd.Parameters.AddWithValue("CyberSec", 0);
+                cmd.Parameters.AddWithValue("cybersec", 0);
+                cmd.Parameters.AddWithValue("ai", 0);
+                cmd.Parameters.AddWithValue("ui", 0);
+                // cmd.Parameters.AddWithValue("UI", 0);
+                await cmd.ExecuteNonQueryAsync();
+                cmd.Dispose();
+            } ;
+            con.Close();
+
+
         }
     }
 }
